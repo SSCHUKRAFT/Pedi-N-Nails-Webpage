3f1bb80de045de0e3a1cd65cf4d2979b7c43cb45
Sean Schukraft <sschukra@oswego.edu>
2023-02-04 14:47:30 -0500

    updated services and their prices

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -6,98 +6,123 @@ const Services = () => (
     <div className="services_container">
       <div className="services_title">Services</div>
       <div className="services_column">
-        <div className="classic_nails">Classic Nails</div>
-        <div>Standard Manicure</div>
+        <div className="service_category">Manicures</div>
+        <div>Regular Manicure</div>
         <div>Gel Manicure</div>
-        <div>Dip Manicure</div>
-        <div className="nail_filling">Nail Filling</div>
-        <div>Standard Fill</div>
-        <div>Gel Polish Fill</div>
-        <div>Ombre&apos; Fill</div>
-        <div>Pink &amp; White Fill</div>
-        <div>Acrylic Fill</div>
-        <div className="acrylic_dip_extensions">Acrylic &amp; Dip Extensions</div>
+        <div className="standard_break">&nbsp;</div>
+        <div>Regular Fill</div>
+        <div>Gel Fill</div>
+        <div>Ombre' Fill</div>
+        <div>Pink & White Fill</div>
+        <div className="standard_break">&nbsp;</div>
         <div>Standard Full Set</div>
-        <div>White Tip Full Set</div>
-        <div>Pink &amp; White Full Set</div>
-        <div>Ombre&apos; Full Set</div>
-        <div>Dip Full Set</div>
-        <div>Pink &amp; White Dip</div>
-        <div>Ombre&apos; Dip</div>
-        <div className="pedicures">Pedicures</div>
+        <div>Gel Full Set</div>
+        <div>White/Clear Tips Full Set</div>
+        <div>Pink/White Full Set</div>
+        <div>Ombre' Full Set</div>
+        <div className="standard_break">&nbsp;</div>
+        <div>Standard Dipping Powder</div>
+        <div>Full Set Dipping Powder</div>
+        <div>Pink/White Dipping Powder</div>
+        <div>Ombre' Dipping Powder</div>
+        <div className="standard_break">&nbsp;</div>
+
+        <div className="service_category">Pedicures</div>
         <div>Standard Pedicure</div>
         <div>Deluxe Spa Pedicure</div>
-        <div>Volcano Spa Pedicure</div>
-        <div>Rose Deluxe Spa Pedicure</div>
-        <div className="waxing">Waxing</div>
+        <div>Gel Pedicure</div>
+        <div className="standard_break">&nbsp;</div>
+
+        <div className="service_category">Combo Specials</div>
+        <div>Standard Manicure & Pedicure</div>
+        <div>Gel Manicure & Standard Pedicure</div>
+        <div>Standard Manicure & Gel Pedicure</div>
+        <div>Gel Manicure & Gel Pedicure</div>
+        <div className="standard_break">&nbsp;</div>
+
+        <div className="service_category">Waxing</div>
         <div>Eyebrow Wax</div>
         <div>Chin Wax</div>
         <div>Lip Wax</div>
-        <div className="additional">Additional Services</div>
-        <div>Standard Polish Change</div>
-        <div>Gel Polish Change</div>
-        <div>Shaping</div>
-        <div>Design / French</div>
+        <div>For Any Other Wax, Ask for Pricing</div>
+        <div className="standard_break">&nbsp;</div>
+
+        <div className="service_category">Additional Services</div>
+        <div>Standard Nail Polish Change</div>
+        <div>Standard Toe Nail Polish Change</div>
+        <div>Gel Nail Polish Change</div>
+        <div>Gel Toe Nail Polish Change</div>
+        <div>Nails Shape</div>
         <div>Chrome Color</div>
         <div>Cat-Eyes</div>
-        <div>Acrylic / Dip Repair</div>
+        <div>Gel Removal</div>
         <div>Acrylic Removal</div>
-        <div>Dip Powder Removal</div>
-        <div>Gel Polish Removal</div>
-        <div className="kids">Kids 10 &amp; Under</div>
-        <div>Pedicure &amp; Manicure</div>
+        <div>Dip Powder</div>
+        <div className="standard_break">&nbsp;</div>
+
+        <div className="service_category">Kids 10 & Under</div>
+        <div>Standard Manicure</div>
+        <div>Standard Pedicure</div>
         <div>Gel Manicure</div>
         <div>Gel Pedicure</div>
-        <div>Manicure Polish Change</div>
-        <div>Pedicure Polish Change</div>
-        <div>Nail Design</div>
+        <div>Nail Art/Design</div>
       </div>
       <div className="prices_column">
         <div className="prices_break">&nbsp;</div>
-        <div>$17</div>
+        <div>$20</div>
         <div>$30</div>
-        <div>$50</div>
-        <div className="prices_break">&nbsp;</div>
+        <div className="standard_break">&nbsp;</div>
         <div>$25</div>
-        <div>$35</div>
+        <div>$35+</div>
         <div>$35</div>
         <div>$40</div>
+        <div className="standard_break">&nbsp;</div>
+        <div>$35</div>
+        <div>$45+</div>
+        <div>$40+</div>
+        <div>$55</div>
+        <div>$55</div>
+        <div className="standard_break">&nbsp;</div>
         <div>$40</div>
-        <div className="prices_break">&nbsp;</div>
-        <div>$17</div>
-        <div>$30</div>
-        <div>$50</div>
-        <div>$53</div>
         <div>$50</div>
         <div>$55</div>
         <div>$60</div>
+        <div className="standard_break">&nbsp;</div>
         <div className="prices_break">&nbsp;</div>
         <div>$30</div>
-        <div>$38</div>
+        <div>$40</div>
         <div>$45</div>
-        <div>$48</div>
+        <div className="standard_break">&nbsp;</div>
         <div className="prices_break">&nbsp;</div>
-        <div>$30</div>
-        <div>$38</div>
         <div>$45</div>
+        <div>$60</div>
+        <div>$63</div>
+        <div>$75</div>
+        <div className="standard_break">&nbsp;</div>
         <div className="prices_break">&nbsp;</div>
+        <div>$12</div>
+        <div>$12</div>
+        <div>$10</div>
+        <div>&nbsp;</div>
+        <div className="standard_break">&nbsp;</div>
+        <div className="prices_break">&nbsp;</div>
+        <div>$10</div>
         <div>$15</div>
-        <div>$38</div>
-        <div>Starting at $5</div>
-        <div>Starting at $5</div>
-        <div>$10</div>
-        <div>$10</div>
-        <div>Starting at $5</div>
-        <div>$10</div>
-        <div>$18</div>
-        <div>$8</div>
+        <div>$25</div>
+        <div>$25</div>
+        <div>$05+</div>
+        <div>$10+</div>
+        <div>$10+</div>
+        <div>$05</div>
+        <div>$15</div>
+        <div>$08</div>
+        <div className="standard_break">&nbsp;</div>
         <div className="prices_break">&nbsp;</div>
-        <div>$27</div>
+        <div>$10</div>
+        <div>$15</div>
         <div>$15</div>
         <div>$20</div>
-        <div>$5</div>
-        <div>$7</div>
-        <div>$3</div>
+        <div>$05+</div>
       </div>
     </div>
   </div>
